e0389b25c2f78a98be86a3b246c6dc00bbea7dce
Maria Popa <81176106+mariapopa19@users.noreply.github.com>
2024-11-15 13:43:39 +0000

    added rules book

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ let logAllPairsOfArray = ((array: string[]) => {
     }
 });
  
-logAllPairsOfArray(boxes)
+// logAllPairsOfArray(boxes)
 
 // O(n^2)
 
@@ -31,9 +31,20 @@ function printAllNumbersThenAllPairSums(numbers:number[]) {
     });
 
     console.log('and these are their sums');
-    numbers.forEach(function(secondNumber) {
+    numbers.forEach(function(firstNumber) {
         numbers.forEach(function(secondNumber) {
             console.log(firstNumber + secondNumber);
         });
     });
 }
+
+printAllNumbersThenAllPairSums([1,2,3,4,5]);
+
+// O(n^2)
+
+
+// Rule Book
+// Rule 1: Worst Case
+// Rule 2: Remove Constants
+// Rule 3: Different terms for inputs 
+// Rule 4: Drop Non Dominants
