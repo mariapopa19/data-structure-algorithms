42b8386081f3eeea1194e6a2c8652b229e810c38
Maria Popa <81176106+mariapopa19@users.noreply.github.com>
2024-11-25 14:35:34 +0000

    hash tables section done

--- Section 7 Hash Tables/README.md/implement-hash-tables.ts
+++ Section 7 Hash Tables/README.md/implement-hash-tables.ts
@@ -13,4 +13,49 @@ class HashTable {
 
         return hash;
     }
+
+    set(key: string, value: number) {
+        let address = this._hash(key);
+        if(!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value]);
+        return this.data;
+    }
+
+    get(key: string): any {
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        console.log(currentBucket);
+        if(currentBucket) {
+            for(let i = 0; i < currentBucket.length; i++) {
+                if(currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+            }
+        } //O(1)
+
+        return undefined;
+    }
+
+    keys(): any {
+        if(!this.data.length) {
+            return undefined;
+        }
+        const keysArray: any[] = [];
+        for(let i = 0; i < this.data.length; i++) {
+            if(this.data[i]) {
+                keysArray.push(this.data[i][0][0]);
+            }
+        }
+    }
+
 }
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+myHashTable.set('apples', 54);
+myHashTable.set('oranges', 2);
+myHashTable.get('grapes');
+console.log(myHashTable.keys());
+// console.log(myHashTable.get('grapes'));
